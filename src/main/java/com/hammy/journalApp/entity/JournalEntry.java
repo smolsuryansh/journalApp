8796d7f7cb952d8e985a499688929636563eaee4
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-08-10 21:43:39 +0530

    add server status and lombok

--- src/main/java/com/hammy/journalApp/entity/JournalEntry.java
+++ src/main/java/com/hammy/journalApp/entity/JournalEntry.java
@@ -1,5 +1,7 @@
 package com.hammy.journalApp.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +10,8 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
+@NoArgsConstructor
 public class JournalEntry {
 
     @Id
@@ -18,37 +22,5 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
 }
--- src/main/java/com/hammy/journalApp/service/JournalEntryService.java
+++ src/main/java/com/hammy/journalApp/service/JournalEntryService.java
@@ -15,20 +15,29 @@ public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
+
     public void saveEntry(JournalEntry journalEntry) {
+
         journalEntryRepository.save(journalEntry);
+
     }
 
     public List<JournalEntry> getAll() {
+
         return journalEntryRepository.findAll();
+
     }
 
     public Optional<JournalEntry> findById(ObjectId id) {
+
         return journalEntryRepository.findById(id);
+
     }
 
     public void deleteById(ObjectId id) {
+
         journalEntryRepository.deleteById(id);
+
     }
 
 }
