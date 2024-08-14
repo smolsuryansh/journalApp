92bcf1622f519585c5ab7f608edabcbebe30028f
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-08-15 00:26:16 +0530

    add builder to User entity

--- src/main/java/com/hammy/journalApp/entity/User.java
+++ src/main/java/com/hammy/journalApp/entity/User.java
@@ -1,21 +1,20 @@
 package com.hammy.journalApp.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "users")
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -32,5 +31,4 @@ public class User {
     private List<JournalEntry> journalEntries = new ArrayList<>();
 
     private List<String> roles;
-
 }
