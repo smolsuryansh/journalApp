9b71458391c47e4101d5259dde6dd9b470a72ff4
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-08-14 17:44:32 +0530

    junit test

---
+++ src/test/java/com/hammy/journalApp/service/UserServiceTests.java
@@ -0,0 +1,43 @@
+package com.hammy.journalApp.service;
+
+import com.hammy.journalApp.entity.User;
+import com.hammy.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "ram",
+            "soryons",
+            "random"
+    })
+    public void testFindByUsername(String name) {
+        assertNotNull(userRepository.findByUserName(name));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,2,4",
+            "3,3,9"
+    })
+    public void test(int a, int b, int expected) {
+        assertEquals(expected, a + b);
+    }
+}
